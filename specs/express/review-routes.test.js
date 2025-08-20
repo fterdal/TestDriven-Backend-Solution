f29f8ace3fd7b0f20225a2d13d33b9b940d5e8ef
Finn Terdal <finnterdal@gmail.com>
2025-08-20 02:35:04 -0400

    clean up

--- specs/express/review-routes.test.js
+++ specs/express/review-routes.test.js
@@ -20,35 +20,25 @@ describe("Review Routes", () => {
     app.use("/api", apiRouter);
   });
 
-  // Sample very basic test just to show the anatomy of a test
-  describe("Basic Arithmetic", () => {
-    it("should be able to add two", () => {
-      expect(BasicArithmetic.addTwo(2)).toBe(4);
-    });
-    it("should be able to subtract two", () => {
-      expect(BasicArithmetic.subtractTwo(2)).toBe(0);
-    });
-  });
-
-
   describe("Review Routes", () => {
-  it("should return all reviews", async () => {
-    const response = await request(app).get("/api/reviews");
-    expect(response.status).toBe(200);
-    expect(response.body.length).toBe(3);
-  });
-
-  it("should return a single review", async () => {
-    const response = await request(app).get("/api/reviews/1");
-    expect(response.status).toBe(200);
-    expect(response.body.text).toBe("This is a review");
-  });
-
-  it("should create a new review", async () => {
-    const response = await request(app).post("/api/reviews").send({
-      text: "Test Review",
+    it("should return all reviews", async () => {
+      const response = await request(app).get("/api/reviews");
+      expect(response.status).toBe(200);
+      expect(response.body.length).toBe(3);
     });
-    expect(response.status).toBe(201);
-    expect(response.body.text).toBe("Test Review");
+
+    it("should return a single review", async () => {
+      const response = await request(app).get("/api/reviews/1");
+      expect(response.status).toBe(200);
+      expect(response.body.text).toBe("This is a review");
+    });
+
+    it("should create a new review", async () => {
+      const response = await request(app).post("/api/reviews").send({
+        text: "Test Review",
+      });
+      expect(response.status).toBe(201);
+      expect(response.body.text).toBe("Test Review");
+    });
   });
 });
